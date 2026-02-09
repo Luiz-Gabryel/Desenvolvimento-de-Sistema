fd1312106d7926bfff6e24c6943678c2bc6eeb31
LuizGabryel <luizgabryel515@gmail.com>
2026-02-08 22:15:58 -0300

    Move arquivos para pasta coisas-aleatorias e adiciona desafios do curso em vídeo

---
+++ Python/feito-por-Luiz/curso-em-video/atividade/atividade1.py
@@ -0,0 +1,5 @@
+nome = input ('Qual seu nome? ')
+idade = input ('Qual sua idade? ')
+peso = input ('Qual seu peso? ')
+
+print(f'seu nome é {nome}, Você tem {idade} Anos, Você tem {peso}')
---
+++ Python/feito-por-Luiz/curso-em-video/desafios/desafio3.py
@@ -0,0 +1,6 @@
+n1 = input ('Primeiro número: ')
+n2 = input ('Segundo número: ')
+
+print('n1' + 'n2') #vai juntar os numeros n1n2
+
+print(n1 + n2) #vai somar
