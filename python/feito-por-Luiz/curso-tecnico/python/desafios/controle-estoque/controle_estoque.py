4e45c6845be8c4b27c3288b756c2576600915ffc
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-02-14 20:57:55 -0300

    att

--- python/feito-por-Luiz/curso-tecnico/python/desafios/controle-estoque/controle_estoque.py
+++ python/feito-por-Luiz/curso-tecnico/python/desafios/controle-estoque/controle_estoque.py
@@ -1,42 +1,36 @@
 #Sistema de Controle de Estoque de Livros, com login e senha, e com opções de adicionar e ver status de todos os livros.
 #ainda fazendo ajustes, ainda não está completo.
 #luiz gabryel 2026 fev
-
 print("Seja bem vindo ao Controle de Estoque de Livros")
-print("-" * 30)
 # Pede usuário e senha
 user = input("Digite seu usuário: ")
 senha = input("Digite sua senha: ")
-print("-" * 30)
 if user == "" or senha == "":
     print("Por favor, tente novamente, faltou algo")
     exit()
 else:
     print(f"Seja bem vindo, {user}")
-print("-" * 30)
 livros = []  # lista onde fica livros
 
 while True: #opções
     print("--- Sistema ---")
+
     print("1 Ver todos os livros")
     print("2 Adicionar um livro")
     print("3 Deslogar")
-    print("-" * 30)
+
     opcao = input("Diigite sua opção:")
-    print("-" * 30)
+
     if opcao == "1":  # olha os livros
         print("--- Livros ---")
+    
         if not livros:
             print("Nenhum livro cadastrado.")
         else:
             for livro in livros:
                 print(livro)
-        print("- Fim -")
+        print("--- Fim ---")
                 
-        #for livro in livros:
-            #print(livro)
-        #print("- Fim -")
-
     elif opcao == "2":  #add livro
         livro = input("Digite o nome do livro: ")
         if livro == "":
@@ -52,9 +46,3 @@ while True: #opções
             break
         else:
             print("Senha errada, tente novamente")
-
-#So parte de codigo antigo
-
-  #for livro in livros:
-            #print(livro)
-        #print("- Fim -")
