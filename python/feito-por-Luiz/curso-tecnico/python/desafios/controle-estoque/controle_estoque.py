9ae5b368e8745b314d03dd4f264628d254131227
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-02-14 12:26:54 -0300

    a

--- python/feito-por-Luiz/curso-tecnico/python/desafios/controle-estoque/controle_estoque.py
+++ python/feito-por-Luiz/curso-tecnico/python/desafios/controle-estoque/controle_estoque.py
@@ -3,26 +3,27 @@
 #luiz gabryel 2026 fev
 
 print("Seja bem vindo ao Controle de Estoque de Livros")
-
+print("-" * 30)
 # Pede usuário e senha
 user = input("Digite seu usuário: ")
 senha = input("Digite sua senha: ")
-
+print("-" * 30)
 if user == "" or senha == "":
     print("Por favor, tente novamente, faltou algo")
     exit()
 else:
     print(f"Seja bem vindo, {user}")
-
+print("-" * 30)
 livros = []  # lista onde fica livros
 
 while True: #opções
-    print("Sistema")
+    print("--- Sistema ---")
     print("1 Ver todos os livros")
     print("2 Adicionar um livro")
     print("3 Deslogar")
+    print("-" * 30)
     opcao = input("Diigite sua opção:")
-
+    print("-" * 30)
     if opcao == "1":  # olha os livros
         print("--- Livros ---")
         if not livros:
