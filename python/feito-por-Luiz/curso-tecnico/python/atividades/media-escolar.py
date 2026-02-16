1a7b123ebf1f421939286e245341e4921b4166c0
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-02-16 20:43:11 -0300

    att

--- python/feito-por-Luiz/curso-tecnico/python/atividades/media-escolar.py
+++ python/feito-por-Luiz/curso-tecnico/python/atividades/media-escolar.py
@@ -1,5 +1,6 @@
 n1 = float (input ('Qual sua primeira nota: '))
 n2 = float (input ('Qual sua segunda nota: '))
+n3 = float (input ('Qual sua terceira nota: '))
 m = (n1 + n2) / 2
 
 print(f"sua nota final e {m}")
