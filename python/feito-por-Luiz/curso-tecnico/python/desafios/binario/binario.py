a4133fda6626f600afa2dae93492f2cef661d70f
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-02-15 22:19:19 -0300

    codigo em binario

---
+++ python/feito-por-Luiz/curso-tecnico/python/desafios/binario/binario.py
@@ -0,0 +1,12 @@
+#ele pega e transforma em binario
+#luiz gabryel 2026 fev
+
+texto = input("digite qualquer coisa: ")
+
+binario = ''
+for letra in texto: # pega letra por letra
+    numero = ord(letra) # transforma em numero
+    bin_letra = format(numero, '08b')  # faz o número em 8 zeros e uns
+    binario += bin_letra + ' ' # junta em linha e espaço 
+
+print(f"Ficou assim: {binario}")
