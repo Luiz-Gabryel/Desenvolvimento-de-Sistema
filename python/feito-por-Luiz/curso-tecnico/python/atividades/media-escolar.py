044e77a16e90d70af2d911e065d05dd57602a4db
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-02-17 11:36:51 -0300

    att

--- python/feito-por-Luiz/curso-tecnico/python/atividades/media-escolar.py
+++ python/feito-por-Luiz/curso-tecnico/python/atividades/media-escolar.py
@@ -4,9 +4,10 @@ n3 = float (input ('Qual sua terceira nota: '))
 n4 = float (input ('Qual sua quarta nota: '))
 m = (n1 + n2 + n3 + n4) / 4
 
-if m <= 7:
-    print("Sua nota e B")
+if m >= 7:
+    print("Sua nota e MB")
 elif m <= 5:
     print("Sua nota e I")
+
 
 print(f"sua nota final e {m}")
