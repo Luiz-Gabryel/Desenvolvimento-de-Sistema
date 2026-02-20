cd3d618cc8cdb4fe0ccb0834a97fd832a5f5a96b
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-02-20 11:21:18 -0300

    correção

--- python/feito-por-Luiz/curso-tecnico/python/desafios/controle-estoque/controle_estoque.py
+++ python/feito-por-Luiz/curso-tecnico/python/desafios/controle-estoque/controle_estoque.py
@@ -61,14 +61,14 @@ while True: #opções
             if livro == remover:
                 encontrado = True #marca como achou
                 confirma = input('Digite sua senha para confirmar: ')
-            if confirma == senha: #se senha for a mesma do confirmar ele removee
-                livros.remove((livro, quantidade)) #senha certa = remove
-                print(f'O livro {livro} foi removido com sucesso!')
-            else: #senha errada = nao remove
-                print('Senha errada, livro não removido.')
-            break
-            if not encontrado: #se nao achou
-                print('Livro não encontrado.') 
+                if confirma == senha: #se senha for a mesma do confirmar ele removee
+                    livros.remove((livro, quantidade)) #senha certa = remove
+                    print(f'O livro {livro} foi removido com sucesso!')
+                else: #senha errada = nao remove
+                    print('Senha errada, livro não removido.')
+                break
+        if not encontrado: #se nao achou
+            print('Livro não encontrado.') 
                 
     elif opcao == '5':  #deslogar somente se a senha for inserida
         sair = input('Confirme sua senha, para sair: ')
