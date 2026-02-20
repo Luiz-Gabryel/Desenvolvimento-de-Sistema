af0c3f3ec3d165d181a95353f4de4cf837a15abf
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-02-19 21:31:30 -0300

    trocando aspas duplos para simples

--- python/feito-por-Luiz/curso-tecnico/python/desafios/controle-estoque/controle_estoque.py
+++ python/feito-por-Luiz/curso-tecnico/python/desafios/controle-estoque/controle_estoque.py
@@ -2,64 +2,64 @@
 #ainda fazendo ajustes, ainda não está completo.
 #luiz gabryel 2026 fev
 
-print("Seja bem vindo ao Controle de Estoque de Livros")# Pede usuário e senha
+print('Seja bem vindo ao Controle de Estoque de Livros')# Pede usuário e senha
 
-user = input("Digite seu usuário: ")
-senha = input("Digite sua senha: ")
-if user == "" or senha == "": # ele valida se tem algo, se nao tem, nao acessa
-    print("Por favor, tente novamente, faltou algo")
+user = input('Digite seu usuário: ')
+senha = input('Digite sua senha: ')
+if user == '' or senha == '': # ele valida se tem algo, se nao tem, nao acessa
+    print('Por favor, tente novamente, faltou algo')
     exit()
 else:
-    print(f"Seja bem vindo, {user}")
+    print(f'Seja bem vindo, {user}')
 
 livros = []  # lista onde fica livros
 
 while True: #opções
-    print("--- Sistema ---")
-    print("1 Ver todos os livros")
-    print("2 Adicionar um livro")
-    print("3 Procurar um Livro")
-    print("4 Deslogar")
-    print("-"*30)
-    opcao = input("Digite sua opção:")
+    print('--- Sistema ---')
+    print('1 Ver todos os livros')
+    print('2 Adicionar um livro')
+    print('3 Procurar um Livro')
+    print('4 Deslogar')
+    print('-'*30)
+    opcao = input('Digite sua opção:')
 
-    if opcao == "1":  # olha os livros
-        print("--- Livros ---")
+    if opcao == '1':  # olha os livros
+        print('--- Livros ---')
     
         if not livros: #ve se tem livros, se tem, ele mostra
-            print("Nenhum livro cadastrado.")
+            print('Nenhum livro cadastrado.')
         else:
             for livro, quantidade in livros:
-                print(f"{livro} - {quantidade} Livros")
-        print("--- Fim ---")
+                print(f'{livro} - {quantidade} Livros')
+        print('--- Fim ---')
                 
-    elif opcao == "2":
-        livro = input("Digite o nome do livro: ")
-        if livro == "":
-            print("Tente novamente")
+    elif opcao == '2':
+        livro = input('Digite o nome do livro: ')
+        if livro == '':
+            print('Tente novamente')
         else:
-            quantidade = input("Digite a quantidade: ")
+            quantidade = input('Digite a quantidade: ')
             livros.append((livro, quantidade))
-            print(f"O livro {livro} foi adicionado com sucesso!")
+            print(f'O livro {livro} foi adicionado com sucesso!')
 
-    elif opcao == "3":
-        busca = input("Digite o nome do livro: ")
+    elif opcao == '3':
+        busca = input('Digite o nome do livro: ')
         encontrado = False # so muda se tiver um livro foi encontrado
         for livro, quantidade in livros: #ve  toda a lista de livro, pegando o nome e quantidade
             if livro == busca: # se tiver ele vai mostrar livro e quantidade
-                print(f"{livro} - {quantidade} quantidade")
+                print(f'{livro} - {quantidade} quantidade')
                 encontrado = True
                 break
         if not encontrado:
-            print("Livro não encontrado.")
+            print('Livro não encontrado.')
 
-    elif opcao == "4":  #deslogar somente se a senha for inserida
-        sair = input("Confirme sua senha, para sair: ")
+    elif opcao == '4':  #deslogar somente se a senha for inserida
+        sair = input('Confirme sua senha, para sair: ')
         if sair == senha:
-            print(f"Volte sempre...{user}")
+            print(f'Volte sempre...{user}')
             break
         else:
-            print("Senha errada, tente novamente")
+            print('Senha errada, tente novamente')
             
     else:
-        print("Opção inválida, tente novamente")
+        print('Opção inválida, tente novamente')
