48c7966a79a3a6d1af91d9f1200adc26dce3845c
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-02-23 21:19:13 -0300

    att

--- python/feito-por-Viviane/nota.py
+++ python/feito-por-Viviane/nota.py
@@ -2,4 +2,4 @@ n1 = float (input ("Qual sua primeira nota? "))
 n2 = float(input ("Qual sua segunda nota? "))
 m = (n1 + n2) / 2
 
-print(f"Sua nota foi {m}")
+print(f"Sua nota foi {m}") #prit media
