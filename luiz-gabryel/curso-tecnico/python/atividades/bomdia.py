c63ce0ac80e287a4cd864770125f2c1d7e496531
LuizGabryel <luizgabryel515@gmail.com>
2026-03-02 12:32:19 -0300

    Programa onde exibe mensagem dependendo do horario

---
+++ luiz-gabryel/curso-tecnico/python/atividades/bomdia.py
@@ -0,0 +1,20 @@
+#Programa onde dependendo do horario ele exiba a mensagem: Bom Dia, Boa Tarde, Boa Noite
+dia = input ("Qual Periodo é? (Manha, Tarde, Noite) ")
+
+if dia == "Manha" or dia == "manhã":
+    print("Bom Dia")
+elif dia == "tarde":
+    print("Boa Tarde")
+elif dia == "noite":
+    print("Boa Noite")
+else:
+    print("As opções são: Manha, Tarde, Noite")
+
+hora = int(input("Qual horas são: "))
+
+if 0 <= hora < 12:
+    print(f"Agora são {hora}h. Bom dia!")
+elif 12 <= hora < 18:
+    print(f"Agora são {hora}h. Boa tarde!")
+else:
+    print(f"Agora são {hora}h. Boa noite!")
