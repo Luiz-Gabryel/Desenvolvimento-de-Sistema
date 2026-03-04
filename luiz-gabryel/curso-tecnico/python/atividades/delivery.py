c84b25f8ce3aefd22ab0e173113e72090fc57379
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-03-04 12:24:52 -0300

    att

--- luiz-gabryel/curso-tecnico/python/atividades/delivery.py
+++ luiz-gabryel/curso-tecnico/python/atividades/delivery.py
@@ -1,20 +1,20 @@
-#Entrada
+#Entrada.
 preco_pizza = float(input("Valor da pizza: R$ "))
 distancia = float(input("Distância (km): "))
 valor_pago = float(input("Cliente vai pagar: R$ "))
 
-#Configuração
+#Configuração.
 tx_km = 1.50 
 tempo_base = 20
 min_km = 3
 
-#Procesamento 
+#Procesamento. 
 taxa_entrega = distancia * tx_km
 total = preco_pizza + taxa_entrega
 tempo_entrega = tempo_base + (distancia * min_km)
 troco = valor_pago - total
 
-#Saida
+#Saida.
 print("--- Nota ---")
 print(f"Total: R$ {total} (Entrega: R$ {taxa_entrega})")
 print(f"tempo estimado: {tempo_entrega} min")
