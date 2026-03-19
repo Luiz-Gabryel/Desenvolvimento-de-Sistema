0250d55aff894d60f91683517d266b48fb78ae15
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-03-19 08:00:10 -0300

    list

--- luiz-gabryel/curso-tecnico/python/atividades/lista.py
+++ luiz-gabryel/curso-tecnico/python/atividades/lista.py
@@ -1,8 +1,11 @@
-p = [p1,p2,p3,p4]
+
 
 p1 = int (input ("Digite um numero: "))
 p2 = int (input ("Digite um numero: "))
 p3 = int (input ("Digite um numero: "))
+p4 = int (input ("Digite um numero: "))
+
+p = [p1,p2,p3,p4]
 
 
 print(p)
