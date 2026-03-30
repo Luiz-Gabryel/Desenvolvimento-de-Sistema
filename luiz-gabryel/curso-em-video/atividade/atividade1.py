5d45dbce79aab03a1f82ea9c3cf4743afa1abff4
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-03-30 17:21:22 -0300

    removendo coisas inutil

--- luiz-gabryel/curso-em-video/atividade/atividade1.py
+++
@@ -1,5 +0,0 @@
-nome = input ('Qual seu nome? ')
-idade = input ('Qual sua idade? ')
-peso = input ('Qual seu peso? ')
-
-print(f'seu nome é {nome}, Você tem {idade} Anos, Você tem {peso}')
--- luiz-gabryel/curso-em-video/desafios/desafio2.py
+++
@@ -1,5 +0,0 @@
-dia = input ("Dia = ")
-mes = input ("Mes = ")
-ano = input ("Ano = ")
-
-print(f'Você nasceu no dia {dia} de {mes} de {ano}, Acertei, ne?')
