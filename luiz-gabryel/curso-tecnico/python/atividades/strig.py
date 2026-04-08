ffdbe601a79b800754e18393d9dc7202447ed41c
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-04-08 18:07:58 -0300

    att

---
+++ luiz-gabryel/curso-tecnico/python/atividades/strig.py
@@ -0,0 +1,39 @@
+my_str_1 = 'Hello'
+my_str_2 = "World"
+my_str_3 = """Multiline
+string"""
+my_str_4 = '''Another
+multiline
+string'''
+
+msg = 'It\'s a sunny day'
+quote = "She said, \"Hello!\""
+
+print(my_str_1)
+print(my_str_2)
+print(my_str_3)
+print(my_str_4)
+print(msg)
+print(quote)
+
+
+my_str = 'Hello worl'
+
+print('Hello' in my_str)  
+print('hey' in my_str)    
+print('hi' in my_str)    
+print('e' in my_str)  
+print('f' in my_str)  
+
+
+print(len(my_str))
+
+print(my_str[0])
+print(my_str[6])
+print(my_str[-1])
+print(my_str[-2])
+
+greeting = '123'
+t2 = '4567'
+print(greeting)
+print(t2[1:4])
