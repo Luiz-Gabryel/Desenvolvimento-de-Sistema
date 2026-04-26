a07f71e5af668b076a04eb283fcc748e6f3d78cf
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-04-26 13:21:26 -0300

    attt

--- luiz-gabryel/treinos/logica/coordenadas.py
+++ luiz-gabryel/treinos/logica/coordenadas.py
@@ -1,11 +1,21 @@
 posicao = [0, 0, 0]
 
-posicao[0] = float(input("Digite X: "))
-posicao[1] = float(input("Digite y: "))
-posicao[2] = float(input("Digite z: "))
+print("--- Coordenadas ---")
+print("1 = Sim ; 2 = Não")
+escolha = int(input("Quer Digitar Coordenadas? "))
 
-print(f"Sua posição é: X={posicao[0]}, Y={posicao[1]}, Z={posicao[2]}")
+if  escolha == 1:
+    posicao[0] = float(input("Digite X: "))
+    posicao[1] = float(input("Digite y: "))
+    posicao[2] = float(input("Digite z: "))
+    print(f"Sua posição é: X={posicao[0]}, Y={posicao[1]}, Z={posicao[2]}")
 
+
+elif escolha == 2:
+    print("Ok, Adeus")
+
+else:
+    print("Erro")
 
 
 
