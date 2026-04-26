ab27e6fee562ee3239acb60535e3562fde888612
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-04-26 13:16:08 -0300

    teste

---
+++ luiz-gabryel/treinos/logica/coordenadas.py
@@ -0,0 +1,31 @@
+posicao = [0, 0, 0]
+
+posicao[0] = float(input("Digite X: "))
+posicao[1] = float(input("Digite y: "))
+posicao[2] = float(input("Digite z: "))
+
+print(f"Sua posição é: X={posicao[0]}, Y={posicao[1]}, Z={posicao[2]}")
+
+
+
+
+
+
+
+
+
+
+
+
+# x = [0]
+# y = [0]
+# z = [0]
+
+# coordenada_x = float(input("Qual sua coordenada x: "))
+# coordenada_y = float(input("Qual sua coordenada y: "))
+# coordenada_z = float(input("Qual sua coordenada Z: "))
+
+
+# print(f"Sua cordenadas são: {x}, {y}, {z}")
+
+
