763067a0a3b790779f1710cb033bbb74a46ee758
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-04-28 12:01:45 -0300

    multiplicação

---
+++ luiz-gabryel/treinos/logica/while.py
@@ -0,0 +1,6 @@
+numero = int(input("Qual o numero que deseja (1 a 10): "))
+multiplicador = 1
+
+while multiplicador <= 10:
+    print(f"{numero} x {multiplicador} = {numero * multiplicador}")
+    multiplicador += 1  
